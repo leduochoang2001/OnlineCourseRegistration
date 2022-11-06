0393cb34ebc2e4716af863d99c2ba0c6130579fd
leduochoang2001 <hoangithub147@gmail.com>
2022-11-06 11:19:15 +0700

    make it better

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const db = require('./config/database')
+const path = require('path');
 
 const Course = require('./models/Course')
 const RegisterInfo = require('./models/RegisterInfo')
@@ -42,6 +43,15 @@ app.post('/registerinfo', (req, res) => {
     info.save()
 
 })
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./client/build'))
+
+    app.get('*', (res, req) => {
+        res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'))
+    })
+
+}
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
