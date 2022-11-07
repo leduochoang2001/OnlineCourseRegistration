cc152fc40d8bfcbd2aa69784a44497f48c16a967
leduochoang2001 <hoangithub147@gmail.com>
2022-11-07 15:36:37 +0700

    make it better

--- server/src/config/database/index.js
+++ server/src/config/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/courses_dev');
+        await mongoose.connect('mongodb+srv://hoangtn1:4olkOvEv5aRYm6jW@onlcr.kanlprq.mongodb.net/courses_dev?retryWrites=true&w=majority');
         console.log('Connect Successfully!')
     } catch (error) {
         console.log('Connect failure!')
